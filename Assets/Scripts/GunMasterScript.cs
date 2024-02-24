33041113b87a89f0d3c812580c94002708d59801
Raymond Tsai <arethere21@gmail.com>
2024-02-24 14:50:01 -0500

    gun colored particles

--- Assets/Scripts/GunMasterScript.cs
+++ Assets/Scripts/GunMasterScript.cs
@@ -12,6 +12,8 @@ public class GunMasterScript : MonoBehaviour
         BOMB
     }
 
+    public GameObject gunObject;
+
     //make sure all data structures are the same length!
 
     [SerializeField]
@@ -27,11 +29,21 @@ public class GunMasterScript : MonoBehaviour
     private int modesUnlocked;
 
 
+    private ParticleSystem gunParticleSystem;
+    private Material particleMaterial;
+    
+
+    
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         modesUnlocked = 0;
+
+        particleMaterial = gunObject.GetComponent<ParticleSystem>().GetComponent<Renderer>().material;
+        particleMaterial.color = colors[(int)mode];
 
         
     }
@@ -45,12 +57,14 @@ public class GunMasterScript : MonoBehaviour
             mode = GunMode.PISTOL;
             Debug.Log(mode);
             Debug.Log((int)mode);
+            particleMaterial.color = colors[(int)mode];
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2) && modesUnlocked >= 1) {
             mode = GunMode.SHOTGUN;
             Debug.Log(mode);
             Debug.Log((int)mode);
+            particleMaterial.color = colors[(int)mode];
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3) && modesUnlocked >= 2)
@@ -58,6 +72,7 @@ public class GunMasterScript : MonoBehaviour
             mode = GunMode.BOMB;
             Debug.Log(mode);
             Debug.Log((int)mode);
+            particleMaterial.color = colors[(int)mode];
         }
 
         //scroll wheel input
@@ -78,6 +93,8 @@ public class GunMasterScript : MonoBehaviour
             {
                 mode += 1;
             }
+
+            particleMaterial.color = colors[(int)mode];
         }
 
     }
