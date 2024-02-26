357fd78a4dbd4b967fa809d36657d2ce8967c787
Raymond Tsai <arethere21@gmail.com>
2024-02-25 19:00:55 -0500

    layout final scene

--- Assets/Scripts/RatBehavior.cs
+++ Assets/Scripts/RatBehavior.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class RatBehavior : MonoBehaviour
 {
+
+    private Animator animator;
+    private GameObject cheese;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        cheese = GameObject.FindGameObjectWithTag("Cheese");
+
+        if (animator == null || cheese == null)
+        {
+            Debug.Log("rat animator or cheese object not found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(transform.position, cheese.transform.position) < 2f)
+        {
+            animator.SetBool("CloseToCheese", true);
+        } else
+        {
+            animator.SetBool("CloseToCheese", false);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
